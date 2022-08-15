2e9e0a1fd7eab24dee438b2b5f3386b1e0701b58
9ganzi <gutiday14@gmail.com>
2022-08-15 17:03:07 -0400

    attempt to adopt animation
    following flame documentation for SpriteAnimationGroupComponent

--- lib/game/level/level.dart
+++ lib/game/level/level.dart
@@ -6,11 +6,24 @@ import 'package:squish_them_all/game/actors/end.dart';
 import 'package:squish_them_all/game/game.dart';
 import 'package:tiled/tiled.dart';
 
-import '../actors/player.dart';
+import '../actors/player(testing).dart';
+
+enum AnimationStates {
+  idle,
+  run,
+  jump,
+  doubleJump,
+  wallJump,
+  fall,
+  hit,
+}
 
 // The mixin ensures that level can access the parent game class instance using gameRef.
 class Level extends Component with HasGameRef<SquishThemAll> {
   final String levelName;
+
+  // Use late to declare variables that will be initialized later. This will ensure the variable is not null at runtime.
+  late Player _player;
 
   Level(this.levelName) : super();
 
@@ -22,7 +35,7 @@ class Level extends Component with HasGameRef<SquishThemAll> {
     );
     add(level);
 
-    // Given a layer name, this method returns the object layer from the map.
+    // The method returns the object layer from the map.
     final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('SpawnPoints');
 
     if (spawnPointsLayer != null) {
@@ -30,12 +43,20 @@ class Level extends Component with HasGameRef<SquishThemAll> {
       for (final spawnPoint in spawnPointsLayer.objects) {
         switch (spawnPoint.type) {
           case 'Player':
-            final player = Player(
-              gameRef.spriteSheet[0],
-              position: Vector2(spawnPoint.x, spawnPoint.y),
-              size: Vector2(spawnPoint.width, spawnPoint.height),
+            _player = Player(
+              animations: {
+                AnimationStates.idle: await gameRef.loadSpriteAnimation(
+                  'Pink Man - Idle (32x32).png',
+                  SpriteAnimationData.sequenced(
+                    amount: 11,
+                    stepTime: .05,
+                    textureSize: Vector2.all(32),
+                  ),
+                ),
+              },
+              current: AnimationStates.idle,
             );
-            add(player);
+            add(_player);
             break;
           case 'Apple':
             final apple = Apple(
