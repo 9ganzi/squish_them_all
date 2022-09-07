59bf8c1e5b6fbb9469604b1521485c4af84754a1
9ganzi <gutiday14@gmail.com>
2022-09-07 02:48:51 -0400

    updated movement logic
    prevented moving when left and right key are pressed simultaneously

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -30,23 +30,6 @@ class SquishThemAll extends Forge2DGame
     await Flame.device.fullScreen();
     await Flame.device.setLandscape();
 
-    // This will make sure to load and keep a reference to the spritesheet before starting the game.
-    spriteSheet = (await images.loadAll(
-      [
-        'Angry Pig - Idle (36x30).png',
-        'Angry Pig - Walk (36x30).png',
-        'Angry Pig - Hit 1 (36x30).png',
-        'Angry Pig - Run (36x30).png',
-        'Angry Pig - Hit 2 (36x30).png',
-        // 'Checkpoints - End (Idle).png',
-        // 'Checkpoints - End (Pressed) (64x64).png',
-        // 'Fruits - Apple.png',
-        // 'Fruits - Bananas.png',
-        // 'Fruits - Melon.png',
-      ],
-    ))
-        .cast<Image>();
-
     // Basically this viewport makes sure the ratio between width and height is always the same in your game, no matter the platform.
     camera.viewport = FixedResolutionViewport(worldSize * camera.zoom);
 
