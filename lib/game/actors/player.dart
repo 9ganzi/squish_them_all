4c460feb10553f78c7fc79c3972fff9bd519015e
9ganzi <gutiday14@gmail.com>
2022-09-18 12:49:37 -0400

    smooth turn
    applying greater force when making turns

--- lib/game/actors/player.dart
+++ lib/game/actors/player.dart
@@ -24,6 +24,7 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
   int _jumpCount = 0;
   bool _isOnGround = false;
   bool _changeDir = false;
+  bool _acceleratingTurn = false;
   late SpriteAnimationGroupComponent _playerComponent;
 
   Player(this._position, {super.renderBody = false});
@@ -47,31 +48,31 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
       PlayerState.idle: SpriteSheet(
         image: gameRef.images.fromCache('Pink Man - Idle (32x32).png'),
         srcSize: _size,
-      ).createAnimation(row: 0, stepTime: 0.05),
+      ).createAnimation(row: 0, stepTime: .05),
       PlayerState.run: SpriteSheet(
         image: gameRef.images.fromCache('Pink Man - Run (32x32).png'),
         srcSize: _size,
-      ).createAnimation(row: 0, stepTime: 0.05),
+      ).createAnimation(row: 0, stepTime: .05),
       PlayerState.jump: SpriteSheet(
         image: gameRef.images.fromCache('Pink Man - Jump (32x32).png'),
         srcSize: _size,
-      ).createAnimation(row: 0, stepTime: 0.05),
+      ).createAnimation(row: 0, stepTime: .05),
       PlayerState.doubleJump: SpriteSheet(
         image: gameRef.images.fromCache('Pink Man - Double Jump (32x32).png'),
         srcSize: _size,
-      ).createAnimation(row: 0, stepTime: 0.05),
+      ).createAnimation(row: 0, stepTime: .05),
       PlayerState.wallJump: SpriteSheet(
         image: gameRef.images.fromCache('Pink Man - Wall Jump (32x32).png'),
         srcSize: _size,
-      ).createAnimation(row: 0, stepTime: 0.05),
+      ).createAnimation(row: 0, stepTime: .05),
       PlayerState.fall: SpriteSheet(
         image: gameRef.images.fromCache('Pink Man - Fall (32x32).png'),
         srcSize: _size,
-      ).createAnimation(row: 0, stepTime: 0.05),
+      ).createAnimation(row: 0, stepTime: .05),
       PlayerState.hit: SpriteSheet(
         image: gameRef.images.fromCache('Pink Man - Hit (32x32).png'),
         srcSize: _size,
-      ).createAnimation(row: 0, stepTime: 0.05),
+      ).createAnimation(row: 0, stepTime: .05),
     };
 
     _playerComponent = SpriteAnimationGroupComponent<PlayerState>(
@@ -83,18 +84,6 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
 
     add(_playerComponent);
   }
-
-  // void idle() {
-  //   _direction = 0;
-  // }
-
-  // void walkLeft() {
-  //   _direction += -1;
-  // }
-
-  // void walkRight() {
-  //   _direction += 1;
-  // }
 
   void jump() {
     if (_jumpCount > 2 ||
@@ -111,6 +100,8 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
   void update(double dt) {
     super.update(dt);
 
+    // print(body.linearVelocity.x);
+
     final velocity = body.linearVelocity.clone();
     final position = body.position;
 
@@ -121,9 +112,9 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
       if (velocity.y > .1) {
         _playerComponent.current = PlayerState.fall;
       }
-    } else if (velocity.y < 0.1 &&
+    } else if (velocity.y < .1 &&
         _playerComponent.current != PlayerState.jump) {
-      if (_direction != 0) {
+      if (velocity.x != 0) {
         _playerComponent.current = PlayerState.run;
       } else {
         _playerComponent.current = PlayerState.idle;
@@ -131,23 +122,30 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
     }
 
     if (_direction != 0) {
-      // body.applyForce(Vector2(_accelerationX, velocity.y));
-      if (velocity.x * velocity.x < _maxSpeed2) {
-        body.applyForce(Vector2(_direction, 0));
-      } else if (_changeDir) {
-        body.applyForce(Vector2(_direction * 25, 0));
-        _changeDir = false;
+      // velocity is slower or equalt to the _maxSpeed
+      if (!(velocity.x * velocity.x > _maxSpeed2)) {
+        // when you are not making a turn
+        if (!_changeDir && !_acceleratingTurn) {
+          if (!(velocity.x * velocity.x == _maxSpeed2)) {
+            body.applyForce(Vector2(_direction, 0));
+          }
+          // print("steady force");
+          //  when you are making a turn
+        } else {
+          // apply bigger force to make the turn faster
+          body.applyForce(Vector2(_direction * 2.5, 0));
+          if (velocity.x * velocity.x >= _maxSpeed2 * .5) {
+            _acceleratingTurn = true;
+          } else {
+            _changeDir = false;
+          }
+        }
       }
     }
-    // else {
-    //   // print("_direction = $_direction");
-    //   body.linearVelocity = Vector2(0, velocity.y);
-    // }
     if (velocity.x * velocity.x > _maxSpeed2) {
       body.linearVelocity =
           Vector2(body.linearVelocity.x.sign * _maxSpeed, velocity.y);
-      // body.applyForce(
-      //     velocity.normalized() * -1 * (velocity.length - _maxSpeed));
+      _acceleratingTurn = false;
     }
 
     if (position.x > worldSize.x) {
@@ -183,31 +181,21 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
 
     if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
       _direction -= 1;
-    } else if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
+    }
+    if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
       _direction += 1;
-    } else {
+    }
+    if (!keysPressed.contains(LogicalKeyboardKey.arrowLeft) &&
+        !keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
       body.linearVelocity.x = 0;
     }
-
-    // if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
-    //   _direction -= 1;
-    // }
-    // if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
-    //   _direction += 1;
-    // }
-    // if (keysPressed.contains(LogicalKeyboardKey.arrowLeft) &&
-    //     keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
-    //   body.linearVelocity.x = 0;
-    // }
 
     return false;
   }
 
   @override
   void beginContact(Object other, Contact contact) {
-    if (other is Wall) {
-      // _accelerationX = 0;
-    }
+    if (other is Wall) {}
   }
 
   @override
@@ -219,12 +207,6 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
       type: BodyType.dynamic,
     );
 
-    // final shape1 = CircleShape()
-    //   ..radius = (_size.x / 2 - 7) / 100
-    //   ..position.setFrom(
-    //     Vector2(0, .02),
-    //   );
-
     final shape = PolygonShape()
       ..setAsBox(
         (_size.x / 2 - 9.5) / 100,
@@ -233,31 +215,13 @@ class Player extends BodyComponent with KeyboardHandler, ContactCallbacks {
         0,
       );
 
-    // final shape3 = CircleShape()
-    //   ..radius = (_size.x / 2 - 8) / 100
-    //   ..position.setFrom(
-    //     Vector2(0, .083),
-    //   );
-
-    // final fixtureDef1 = FixtureDef(shape1)
-    //   ..density = 150000
-    //   ..friction = 0
-    //   ..restitution = 0;
-
     final fixtureDef = FixtureDef(shape)
       ..density = 15
       ..friction = 0
       ..restitution = 0;
 
-    // final fixtureDef3 = FixtureDef(shape3)
-    //   ..density = 15
-    //   ..friction = 0
-    //   ..restitution = 0;
-
     return world.createBody(bodyDef)
-      // ..createFixture(fixtureDef1)
       ..createFixture(fixtureDef)
-      // ..createFixture(fixtureDef3)
       ..setFixedRotation(true);
   }
 }
