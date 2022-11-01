c008adf24e24255c6bfbc31451b7e53073b84c44
BetrayerMagik <104353299+BetrayerMagik@users.noreply.github.com>
2022-10-31 22:27:35 -0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+import 'game/game.dart';
+
+import 'package:squish_them_all/game/overlays/game_over.dart';
+import 'package:squish_them_all/game/overlays/main_menu.dart';
+import 'package:squish_them_all/game/overlays/pause_menu.dart';
+import 'package:squish_them_all/game/overlays/settings.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final _game = SquishThemAll();
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Squish Them All!',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: GameWidget<SquishThemAll>(
+          game: kDebugMode ? SquishThemAll() : _game,
+          overlayBuilderMap: {
+            MainMenu.id: (context, game) => MainMenu(gameRef: game),
+            PauseMenu.id: (context, game) => PauseMenu(gameRef: game),
+            GameOver.id: (context, game) => GameOver(gameRef: game),
+            Settings.id: (context, game) => Settings(gameRef: game),
+          },
+          initialActiveOverlays: const [MainMenu.id],
+        ),
+      ),
+    );
+  }
+}
