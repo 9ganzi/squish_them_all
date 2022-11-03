7ca2bc8903ac5b644e9ada5651b26f133a6382e9
BetrayerMagik <104353299+BetrayerMagik@users.noreply.github.com>
2022-11-03 05:51:24 -0400

    Add files via upload

---
+++ lib/game/actors/melon.dart
@@ -0,0 +1,80 @@
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flame/sprite.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:squish_them_all/game/actors/player.dart';
+import 'package:squish_them_all/game/game.dart';
+
+class Melon extends BodyComponent<SquishThemAll> with ContactCallbacks {
+  final _size = Vector2(32, 32);
+  bool isTaken = false;
+
+  late SpriteAnimationComponent _melonComponent;
+
+  final Vector2 position;
+
+  Melon(this.position, {super.renderBody = false});
+
+  @override
+  Future<void> onLoad() async {
+    await gameRef.images.load('Fruits - Melon.png');
+
+    await super.onLoad();
+
+    _melonComponent = SpriteAnimationComponent(
+      animation: SpriteSheet(
+        image: gameRef.images.fromCache('Fruits - Melon.png'),
+        srcSize: _size,
+      ).createAnimation(row: 0, stepTime: 0.05),
+      anchor: Anchor.center,
+      size: _size / 100,
+    );
+
+    add(_melonComponent);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    if (isTaken) {
+      world.destroyBody(body);
+      // // Causes error!
+      // gameRef.remove(this);
+    }
+  }
+
+  void hit() {
+    isTaken = true;
+  }
+
+  @override
+  Body createBody() {
+    // debugMode = true;
+    final bodyDef = BodyDef(
+      userData: this,
+      position: position,
+      type: BodyType.kinematic,
+    );
+
+    final shape = CircleShape()..radius = .05;
+    final fixtureDef = FixtureDef(shape);
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+
+  @override
+  void beginContact(Object other, Contact contact) {
+    if (other is Player) {
+      hit();
+      add(
+        OpacityEffect.fadeOut(
+          LinearEffectController(0.3),
+        )..onFinishCallback = () {
+            add(RemoveEffect());
+          },
+      );
+      gameRef.playerData.score.value += 1;
+    }
+    super.beginContact(other, contact);
+  }
+}
---
+++ lib/game/actors/wall.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:flame_forge2d/flame_forge2d.dart';
+
+import 'package:squish_them_all/game/game.dart';
+
+class Wall extends BodyComponent<SquishThemAll> {
+  final Vector2 _worldSize;
+  final List<EdgeShape> edges;
+
+  Wall(this.edges, this._worldSize);
+
+  @override
+  Body createBody() {
+    // debugMode = true;
+    late Body wall;
+
+    final bodyDef = BodyDef(
+      type: BodyType.static,
+      userData: this,
+    );
+
+    wall = world.createBody(bodyDef);
+
+    final shape1 = EdgeShape()..set(Vector2.zero(), Vector2(0, _worldSize.y));
+    final shape2 = EdgeShape()..set(Vector2.zero(), Vector2(_worldSize.x, 0));
+    final shape3 = EdgeShape()
+      ..set(Vector2(_worldSize.x, 0), Vector2(_worldSize.x, _worldSize.y));
+
+    wall
+      ..createFixtureFromShape(shape1)
+      ..createFixtureFromShape(shape2)
+      ..createFixtureFromShape(shape3);
+
+    return wall;
+  }
+}
